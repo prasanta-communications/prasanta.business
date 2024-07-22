dc33e5747657a593d441dcc89a29de0a1e022ed1
Ravi Teja <raviteja155@gmail.com>
2024-07-22 15:30:37 +0530

    Add contact page.

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Prasanta Communications';
+export const SITE_DESCRIPTION = 'Powering businesses and people with technology.'; 
